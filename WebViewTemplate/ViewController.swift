fca8c1ba68daa75303e834c38213edd7b45fbeeb
WEREMSOFT <pablo.weremczuk@gmail.com>
2016-12-08 15:45:49 -0300

    initial commit

--- WebViewTemplate/ViewController.swift
+++ WebViewTemplate/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myWebView: UIWebView!
+    var urlToLoad:String = "http://weremsoft.net/wall/17qyd6o/123/0/0";
+    @IBAction func ClearCacheButton(_ sender: Any) {
 
+        NSLog(myWebView.stringByEvaluatingJavaScript(from: "localStorage.clear();")!);
+    }
+    
+    @IBAction func btnReload(_ sender: Any) {
+                loadWebView(urlToLoad: self.urlToLoad);
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadWebView(urlToLoad: self.urlToLoad);
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func loadWebView(urlToLoad: String){
+        let url = NSURL (string: urlToLoad);
+        let requestObj = NSURLRequest(url: url! as URL, cachePolicy:NSURLRequest.CachePolicy.reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0);
+        
+        myWebView.loadRequest(requestObj as URLRequest);
+    }
 }
 
